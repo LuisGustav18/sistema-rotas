64c8e3464f6072547c5ce371499d0e4a4cc5e015
LuisGustav18 <luis0gustavo43@gmail.com>
2026-07-14 12:59:45 -0300

    Initialized project

---
+++ src/main/java/com/luis/sistema_rotas/SistemaRotasApplication.java
@@ -0,0 +1,13 @@
+package com.luis.sistema_rotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/sistema_rotas/SistemaRotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.sistema_rotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
